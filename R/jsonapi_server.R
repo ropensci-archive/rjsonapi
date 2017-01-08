ec746729950697e20c99f09f4a996088c2a73d2a
Scott Chamberlain <myrmecocystus@gmail.com>
2017-01-07 16:47:30 -0800

    added more data for server fxn

--- R/jsonapi_server.R
+++ R/jsonapi_server.R
@@ -1,10 +1,42 @@
 #' Start a JSONAPI server
 #'
 #' @export
+#' @param port (integer) the port to run the server on. default: 8000
+#'
+#' @details Right now, this function doesn't take arbitrary input, but
+#' instead serves the same content as this JSON API example
+#' \url{https://github.com/endpoints/endpoints-example}. Note that not all
+#' features are the same as the full \code{endpoints-example}, but most
+#' should work.
+#'
+#' Right now, this function serves data from static, minified JSON files,
+#' so there's no dynamic database underneath.
+#'
+#' Kill the server by CTRL+C, ESC or similar.
+#'
 #' @examples \dontrun{
-#' # jsonapi_server()
+#' # start server in another R session
+#' if (interactive()) {
+#'   jsonapi_server()
+#'
+#'   # Back in this session ...
+#'   # Connect
+#'   (conn <- jsonapi_connect("http://localhost:8000"))
+#'
+#'   # Get data
+#'   conn$url
+#'   conn$version
+#'   conn$content_type
+#'   conn$routes()
+#'   conn$route("authors")
+#'   conn$route("chapters")
+#'   conn$route("authors/1")
+#'   conn$route("authors/1/books")
+#'   conn$route("chapters/5")
 #' }
-jsonapi_server <- function() {
-  r <- plumber::plumb(system.file("examples", "server.R", package = "rjsonapi"))
-  r$run(port = 8000)
+#' }
+jsonapi_server <- function(port = 8000) {
+  r <- plumber::plumb(system.file("examples", "server.R",
+                                  package = "rjsonapi"))
+  r$run(port = port)
 }
--- inst/examples/server.R
+++ inst/examples/server.R
@@ -1,12 +1,87 @@
-#* @get /books
-get_books <- function(){
-  dat <- jsonlite::fromJSON("books.json", FALSE)
-  list(meta = list(total = length(dat)), data = dat, errors = list())
+#* @get /v1/?
+v1 <- function(){
+  lapply(jsonlite::fromJSON("v1.json", FALSE), jsonlite::unbox)
 }
 
-#* @get /books/<book:int>
-get_books <- function(book){
-  dat <- jsonlite::fromJSON("books.json", FALSE)
-  dat <- dat[vapply(dat, "[[", "", "id") == as.character(book)]
-  list(meta = list(total = length(dat)), data = dat, errors = list())
+
+
+#* @get /v1/authors/?
+authors <- function(){
+  jsonlite::fromJSON("authors.json", FALSE)
+}
+
+#* @get /v1/authors/<author:int>/?
+author <- function(author){
+  # jsonlite::fromJSON(sprintf("authors%s.json", author), FALSE)
+  res <- jsonlite::fromJSON("authors.json", FALSE)
+  list(data = res$data[vapply(res$data, "[[", "", "id") == as.character(author)])
+}
+
+#* @get /v1/authors/<author:int>/books/?
+author_book <- function(author){
+  jsonlite::fromJSON(sprintf("authors_%s_books.json", author), FALSE)
+}
+
+
+
+#* @get /v1/books/?
+books <- function(){
+  jsonlite::fromJSON("books.json", FALSE)
+}
+
+#* @get /v1/books/<book:int>?
+book <- function(book){
+  res <- jsonlite::fromJSON("books.json", FALSE)
+  list(data = res$data[vapply(res$data, "[[", "", "id") == as.character(book)])
+}
+
+
+
+#* @get /v1/chapters/?
+chapters <- function(){
+  jsonlite::fromJSON("chapters.json", FALSE)
+}
+
+#* @get /v1/chapters/<chapter:int>?
+chapter <- function(chapter){
+  res <- jsonlite::fromJSON("chapters.json", FALSE)
+  list(data = res$data[vapply(res$data, "[[", "", "id") == as.character(chapter)])
+}
+
+
+#* @get /v1/photos/?
+photos <- function(){
+  jsonlite::fromJSON("photos.json", FALSE)
+}
+
+#* @get /v1/photos/<photo:int>?
+photo <- function(photo){
+  res <- jsonlite::fromJSON("photos.json", FALSE)
+  list(data = res$data[vapply(res$data, "[[", "", "id") == as.character(photo)])
+}
+
+
+
+#* @get /v1/series/?
+series <- function(){
+  jsonlite::fromJSON("series.json", FALSE)
+}
+
+#* @get /v1/series/<ser:int>?
+series <- function(ser){
+  res <- jsonlite::fromJSON("series.json", FALSE)
+  list(data = res$data[vapply(res$data, "[[", "", "id") == as.character(ser)])
+}
+
+
+
+#* @get /v1/stores/?
+stores <- function(){
+  jsonlite::fromJSON("stores.json", FALSE)
+}
+
+#* @get /v1/stores/<store:int>?
+store <- function(store){
+  res <- jsonlite::fromJSON("stores.json", FALSE)
+  list(data = res$data[vapply(res$data, "[[", "", "id") == as.character(store)])
 }
